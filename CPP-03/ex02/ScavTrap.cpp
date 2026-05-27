7567740c989aba95cae33cc43e2613b0a9d6ba9c
Francois Garnier <fgarnier@1F3.42angouleme.fr>
2026-05-27 11:20:15 +0200

    wadaw

---
+++ CPP-03/ex02/ScavTrap.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgarnier <fgarnier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 19:21:53 by fgarnier          #+#    #+#             */
+/*   Updated: 2026/05/27 11:06:39 by fgarnier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() : ClapTrap("Default_Scav") 
+{
+	this->hitPoints = 100;
+	this->energyPoints = 50;
+	this->attackDamage = 20;
+	std::cout << "A default ScavTrap is assembled !" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) 
+{
+	this->hitPoints = 100;
+	this->energyPoints = 50;
+	this->attackDamage = 20;
+	std::cout << "A ScavTrap named " << this->name << " is assembled !" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& src) : ClapTrap(src)
+{
+	std::cout << "ScavTrap Copy constructor called !" << std::endl;
+	*this = src;
+}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap& other)
+{
+	if (this != &other)
+	{
+		ClapTrap::operator=(other);
+		std::cout << "ScavTrap " << this->name << " is copied !" << std::endl;
+	}
+	return *this;
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << this->name << " is destroyed !" << std::endl;
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+	if (!this->status())
+		return;
+	std::cout << "ScavTrap " << this->name << " shoots " << target << ", causing " << this->attackDamage << " points of damage!" << std::endl;
+	this->energyPoints--;
+}
+
+void ScavTrap::guardGate()
+{
+	if (!this->status())
+		return;
+	std::cout << "ScavTrap " << this->name << " is now in GateKeeper mode!" << std::endl;
+}
